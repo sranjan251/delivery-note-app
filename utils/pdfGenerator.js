b5553ea23ebd8ea61be86257109724aa5accbd0c
Sumit Dutta <sumitdutta@Sumits-MacBook-Air-2.local>
2026-02-22 23:42:37 +0530

    render chromium stable fix

--- utils/pdfGenerator.js
+++ utils/pdfGenerator.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 
 async function generatePDF(html){
 
@@ -9,6 +9,8 @@ try{
 browser = await puppeteer.launch({
 
 headless:true,
+
+executablePath:"/usr/bin/chromium",
 
 args:[
 
